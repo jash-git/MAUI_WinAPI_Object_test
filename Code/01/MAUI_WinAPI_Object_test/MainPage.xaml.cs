1de0c4c200a20edc517248e9765af0b6de4507d2
jash.liao <jash.liao@gmail.com>
2023-09-05 11:34:49 +0800

    CREATE

---
+++ Code/01/MAUI_WinAPI_Object_test/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿//using Android.App;
+using Microsoft.Maui.Controls;
+
+namespace MAUI_WinAPI_Object_test;
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+	}
+    private void OnClosed(object sender, EventArgs e)
+    {
+        // Close the active window
+        //https://learn.microsoft.com/en-us/dotnet/maui/fundamentals/windows
+        Application.Current.CloseWindow(GetParentWindow());
+    }
+}
+
