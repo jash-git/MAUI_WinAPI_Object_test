22be8d2dfb65d5557d5d3108a5aea7959fedada0
jash.liao <jash.liao@gmail.com>
2023-09-07 12:57:47 +0800

    Add 05_net7

---
+++ Code/05_net7/MAUI_WinAPI_Object_test/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUI_WinAPI_Object_test
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
