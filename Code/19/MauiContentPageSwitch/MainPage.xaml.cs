232f6392ea88f65c56b37a17c3b46ee75600e04b
jash.liao <jash.liao@gmail.com>
2023-10-31 15:59:46 +0800

    Modified 202310311559

--- Code/19/MauiContentPageSwitch/MainPage.xaml.cs
+++ Code/19/MauiContentPageSwitch/MainPage.xaml.cs
@@ -17,12 +17,17 @@ public partial class MainPage : ContentPage
 			CounterBtn.Text = $"Clicked {count} time";
 		else
 			CounterBtn.Text = $"Clicked {count} times";
-		
-		//---
-		//頁面切換
-        //NavigationPage navigationPage = new NavigationPage(new NewPage1());
-        await Navigation.PushModalAsync(new NewPage1());
+
+        //---
+        //頁面切換
+        var modalPage = new NewPage1();
+        modalPage.Disappearing += (sender2, e2) =>
+        {//您可以訂閱 ContentPage 的 Disappearing 事件。當 ContentPage 消失時，該事件將觸發。您可以使用此事件來執行在 ContentPage 關閉後要執行的操作。
+            count++;//畫面關閉才會被執行
+        };
+        await Navigation.PushModalAsync(modalPage);
         //---頁面切換
+
     }
 }
 
