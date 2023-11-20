197d01ee9cd482ec81deabe1cfd3be4e7e214ba7
jash.liao <jash.liao@gmail.com>
2023-11-20 17:42:30 +0800

    Add 202311201742

---
+++ Code/23/修改後的範例/MauiDataGridSample/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.ObjectModel;
+using System.Net.Http.Json;
+
+namespace MauiDataGridSample
+{
+    // Plugin repo: https://github.com/akgulebubekir/Maui.DataGrid
+    public partial class MainPage : ContentPage
+    {
+        private readonly HttpClient httpClient = new();
+
+        public bool IsRefreshing { get; set; }
+        public ObservableCollection<Monkey> Monkeys { get; set; } = new();
+        public Command RefreshCommand { get; set; }
+        public Monkey SelectedMonkey { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();//jash add
+            RefreshCommand = new Command(async () =>
+            {
+                // Simulate delay
+                //jash stop await Task.Delay(2000);
+
+                await LoadMonkeys();
+
+                IsRefreshing = false;
+                OnPropertyChanged(nameof(IsRefreshing));
+            });
+
+            BindingContext = this;
+
+            //jash stop InitializeComponent();
+        }
+
+        protected async override void OnNavigatedTo(NavigatedToEventArgs args)
+        {
+            base.OnNavigatedTo(args);
+
+            await LoadMonkeys();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Monkeys.Clear();
+        }
+
+        private async Task LoadMonkeys()
+        {
+            var monkeys = await httpClient.GetFromJsonAsync<Monkey[]>("https://montemagno.com/monkeys.json");
+
+            Monkeys.Clear();
+
+            foreach (Monkey monkey in monkeys)
+            {
+                Monkeys.Add(monkey);
+            }
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await LoadMonkeys();
+        }
+    }
+}
---
+++ Code/23/修改後的範例/README.md
@@ -0,0 +1,4 @@
+[![YouTube Video Views](https://img.shields.io/youtube/views/ERQMKw26zrs?style=social)](https://youtu.be/ERQMKw26zrs)
+
+# .NET MAUI DataGrid Sample
+Sample code to demonstrate how to implement [Maui.DataGrid](https://github.com/akgulebubekir/Maui.DataGrid) in a .NET MAUI application
